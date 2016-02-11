35036f64e1cad2f2982cb2f44a171a1c66dd0ff9
Anton Salnik <skanton@gmail.com>
2016-02-11 17:26:40 +0600

    Package Eclipse IDE for Java Developers with Sweet.

---
+++ README.md
@@ -0,0 +1,68 @@
+The EPP Build
+=============
+
+The [Eclipse Packaging Project (EPP)](http://www.eclipse.org/epp/) provides 
+the download packages based on the content of the yearly Simultaneous Release. 
+The download packages are provided from 
+[www.eclipse.org/downloads/](http://www.eclipse.org/downloads/).
+
+Build a Package Locally
+-----------------------
+
+It's *easy* to run the build locally! All you need is Maven and then you need 
+to tell Maven which package(s) to build via profile. As an example, the following 
+command from the root of the Git repository builds the RCP/RAP package against 
+the Simultaneous Release staging p2 repository:
+
+    mvn clean verify -Psweet.package.java
+
+This build creates output in two places:
+
+1. tar.gz/zip archives with the packages in `archive/` and
+2. a p2 repository with the EPP artifacts in `archive/repository/`.
+
+Windows users
+------------- 
+
+If you are running the build on Windows, the last build step will currently fail. 
+This failure can be circumvented by skipping the last step which aggregates the 
+filtered EPP artifacts from the packages into a new p2 repository. For further 
+details see [bug 426416](https://bugs.eclipse.org/bugs/show_bug.cgi?id=426416).
+At the moment it is advised to run the build command on Windows with `package` 
+only:
+
+    mvn clean package -Psweet.package.java
+
+In addition to that it is not possible to create zip and tar.gz archives on 
+Windows due to missing Bash scripting capabilities. On Windows, the output of the
+build is the `eclipse` directory that contains the usual content from the zip
+archive. This directory can be found below (e.g. RCP package) 
+`packages/org.eclipse.epp.package.rcp.product/target/products/`.
+
+Available Profiles
+------------------
+
+Each package uses its own profile:
+
+- sweet.package.java
+
+With the signing profiles enabled, the build artifacts (bundles, features) and the
+Windows and Mac OSX executables are signed. This is done by using the Eclipse Foundation 
+internal signing service and can be activated only if the build is running there.
+
+- eclipse-sign-jar profile enables signing of the EPP bundles and jar files
+- eclipse-sign-mac profile enables usage of Mac OSX signing service
+- eclipse-sign-windows profile enables usage of Windows signing service
+
+Additional Configuration Possibilities
+--------------------------------------
+
+By default, the EPP build uses the content of the Eclipse Simultaneous Release *Staging*
+repository at <http://download.eclipse.org/releases/staging/> as input. Sometimes it is
+desired to build against another release (e.g. a different milestone), or against a local
+mirror of this repository. This can be achieved by setting the Java property
+`eclipse.simultaneous.release.repository`to another URL. As an example, by adding the
+following argument to the Maven command line, the EPP build will read its input from the
+composite Eclipse Mars repository:
+
+    -Declipse.simultaneous.release.repository="http://download.eclipse.org/releases/mars"
---
+++ releng/org.eclipse.epp.config/tools/releaseRename.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+RELEASETRAIN=mars
+RELEASEDIRECTORY=/home/data/httpd/download.eclipse.org/technology/epp/downloads/release
+TESTDIRECTORY=/shared/technology/epp/epp_build/${RELEASETRAIN}/download
+CURRENTDIR=${PWD}
+
+
+#############################################################################
+
+if [ -z ${2} ]
+then
+  echo "ERROR: At least two parameters (build id and target version) are necessary. Stopping."
+  echo "       Example: \"sh releaseRename.sh 20080117-0620 M5\""
+  exit 1
+fi
+TESTBUILDID=${1}
+TARGETVERSION=${2}
+
+echo "Running the releaseRename script for ${RELEASETRAIN} with build ${TESTBUILDID} and version ${TARGETVERSION}"
+
+SOURCEDIR=${TESTDIRECTORY}/${TESTBUILDID}
+echo -n "Checking source directory: "
+if [ ! -d ${SOURCEDIR} ]
+then
+  echo "failed"
+  echo "ERROR: ${SOURCEDIR} does not exist. Stopping."
+  exit 1
+fi
+echo "okay"
+
+TARGETDIR=${RELEASEDIRECTORY}/${RELEASETRAIN}/${TARGETVERSION}
+echo -n "Checking target directory: "
+if [ -d ${TARGETDIR} ]
+then
+  echo "failed"
+  echo "ERROR: ${TARGETDIR} does already exist. Stopping."
+  exit 1
+fi
+echo "okay"
+
+echo 1st: Create the release directory ${TARGETDIR}
+mkdir ${TARGETDIR}
+
+echo 2nd: Copy logfiles
+cp -a ${SOURCEDIR}/*.log ${TARGETDIR}
+
+echo 3rd: Copy XML config files: renamed feature.xml and package configuration files
+cp -a ${SOURCEDIR}/*.xml ${TARGETDIR}
+
+echo 4th: Copy and rename packages
+cd ${SOURCEDIR}
+for II in *eclipse*; do
+  if [[ ! ( "${II}" =~ ".sha1" || "${II}" =~ ".md5" || "${II}" =~ "^eclipse_" ) ]]
+  then
+    NEWNAME=`echo ${II} | \
+             cut -d "_" -f 2- | \
+             sed 's/linux\.gtk\.x86\_64/linux-gtk-x86\_64/' | \
+             sed 's/linux\.gtk\.x86\./linux\-gtk\./' | \
+             sed 's/win32\.win32\.x86\./win32\./' | \
+             sed 's/win32\.win32\.x86\_64\./win32\-x86\_64\./' | \
+             sed 's/macosx\.cocoa\.x86\_64/macosx\-cocoa-x86\_64/'`
+    echo .. Copying ${II} to ${TARGETDIR}/${NEWNAME}
+    rsync -av ${II} ${TARGETDIR}/${NEWNAME}
+    if [ $? = "0" ]; then
+      echo .... Successfully copied
+    else
+      echo Trying again...
+      rsync -av --bwlimit=400 ${II} ${TARGETDIR}/${NEWNAME}
+    fi
+  fi
+done
+
+echo 5th: Adjust package names with incubating components
+cd ${TARGETDIR}
+# pattern to match: <product name="eclipse-linuxtools-juno-RC5-incubation" /> -> "eclipse-linuxtools-juno-RC5"
+INCUBATION=`ls *.xml | grep -v feature | xargs grep "product name=\"eclipse.*incubation" | sed 's/^.*\(eclipse-.*\)-incubation.*/\1/'`
+echo Found ${INCUBATION} in incubation
+for II in ${INCUBATION}; do
+  echo ".. Renaming ${II} incubating packages"
+  for INCUBATIONPACKAGE in `ls *${II}* | grep -v "\.md5$" | grep -v "\.sha1$" | grep -v "incubation"`; do
+    INCUBATIONPACKAGE_FILE=`echo ${INCUBATIONPACKAGE} | sed 's:\(.*\)\('${II}'\)\(.*\):\1\2-incubation\3:'`
+    echo -n ".... Moving ${INCUBATIONPACKAGE} to ${INCUBATIONPACKAGE_FILE}"
+    mv ${INCUBATIONPACKAGE} ${INCUBATIONPACKAGE_FILE}
+    echo " done."
+  done
+done
+
+echo "6th: Update release string in archive file names"
+cd ${TARGETDIR}
+for II in `ls *eclipse-*.tar.gz *eclipse-*.zip`; do
+  # 20110615-0608_eclipse-testing-juno-RC5-macosx.cocoa.x86_64.tar.gz
+  # eclipse-parallel-juno-RC4-incubation-macosx-cocoa-x86_64.tar.gz
+  NEWNAME=`echo $II | sed 's:^\(.*eclipse\-\)\([a-z]*\-\)\([a-z]*\-\)\([A-Z,0-9]*\)\(\-.*\)$:\1\2\3'${TARGETVERSION}'\5:'`
+  echo -n ".. Updating $II with $NEWNAME"
+  mv ${II} ${NEWNAME}
+  echo " done."
+done
+
+echo 7th: Re-calculate checksum files
+cd ${TARGETDIR}
+for II in eclipse*.zip eclipse*.tar.gz; do 
+  echo .. $II
+  md5sum $II >$II.md5
+  sha1sum $II >$II.sha1
+done
+
+echo Moving to release directory ${TARGETDIR} done.
+exit 0
